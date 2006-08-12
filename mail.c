3e57326bc8352457d67260fd843c23fd454d689b
Nicholas Marriott <nicholas.marriott@gmail.com>
2006-08-12 09:26:52 +0000

    Style nit.

--- mail.c
+++ mail.c
@@ -1,4 +1,4 @@
-/* $Id: mail.c,v 1.1 2006-08-11 17:46:49 nicm Exp $ */
+/* $Id: mail.c,v 1.2 2006-08-12 09:26:52 nicm Exp $ */
 
 /*
  * Copyright (c) 2006 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -27,8 +27,7 @@ trim_from(struct mail *m)
 {
 	char	*ptr;
 
-	if (m->data == NULL || m->size < 5 || 
-	    strncmp(m->data, "From ", 5) != 0)
+	if (m->data == NULL || m->size < 5 || strncmp(m->data, "From ", 5) != 0)
 		return;
 	
 	ptr = memchr(m->data, '\n', m->size);
