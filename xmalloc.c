54ab254c697d36100a948f094f965b77f5264fd6
Nicholas Marriott <nicholas.marriott@gmail.com>
2006-11-23 23:59:57 +0000

    Trim these again.

--- xmalloc.c
+++ xmalloc.c
@@ -1,4 +1,4 @@
-/* $Id: xmalloc.c,v 1.22 2006-11-23 22:43:34 nicm Exp $ */
+/* $Id: xmalloc.c,v 1.23 2006-11-23 23:59:57 nicm Exp $ */
 
 /*
  * Copyright (c) 2004 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -69,17 +69,17 @@ xmalloc_dump(const char *hdr)
 	char	 		 tmp[4096];
 	struct xmalloc_block	*p;
 	
-	log_debug2("xmalloc_dump: %s: allocated=%zu, freed=%zu, "
-	    "difference=%zd, peak=%zd", hdr, xmalloc_allocated, xmalloc_freed,
+	log_debug2("%s: allocated=%zu, freed=%zu, difference=%zd, peak=%zd", 
+	    hdr, xmalloc_allocated, xmalloc_freed,
 	    xmalloc_allocated - xmalloc_freed, xmalloc_peak);
-	log_debug2("xmalloc_dump: %s: mallocs=%u, reallocs=%u, frees=%u", hdr,
+	log_debug2("%s: mallocs=%u, reallocs=%u, frees=%u", hdr,
 	    xmalloc_mallocs, xmalloc_reallocs, xmalloc_frees);
 
 	if (xmalloc_allocated == xmalloc_freed)
 		return;
 
 	len = sizeof tmp;
-	if ((off = xsnprintf(tmp, len, "xmalloc_dump: %s: ", hdr)) < 0)
+	if ((off = xsnprintf(tmp, len, "%s: ", hdr)) < 0)
 		fatal("xsnprintf");
 	for (i = 0; i < XMALLOC_SLOTS; i++) {
 		n++;
