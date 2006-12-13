4f7867007c6600ccc026ffbf5925f65783ebbc9d
Nicholas Marriott <nicholas.marriott@gmail.com>
2006-12-13 20:34:41 +0000

    These must be static now...

--- replace.c
+++ replace.c
@@ -1,4 +1,4 @@
-/* $Id: replace.c,v 1.17 2006-12-13 19:58:58 nicm Exp $ */
+/* $Id: replace.c,v 1.18 2006-12-13 20:34:41 nicm Exp $ */
 
 /*
  * Copyright (c) 2006 Nicholas Marriott <nicm@users.sourceforge.net>
@@ -27,7 +27,7 @@ void	initmap(char *[REPL_LEN], struct account *, struct action *, char *);
 void
 initmap(char *map[REPL_LEN], struct account *a, struct action *t, char *s)
 {
-	char		 H[5], M[5], S[5], d[5], m[5], y[5], W[5], Y[5], Q[5];
+	static char	 H[5], M[5], S[5], d[5], m[5], y[5], W[5], Y[5], Q[5];
 	struct tm	*tm;
 	time_t		 tt;
 
@@ -74,7 +74,7 @@ replacepmatch(char *src, struct account *a, struct action *t, char *s,
 	size_t	 len;
 	u_int	 i;
 
-	memset(map, 0, sizeof map);
+	memset(map, 0, REPL_LEN * sizeof (char *));
 	initmap(map, a, t, s);
 
 	for (i = 0; i < NPMATCH; i++) {
@@ -102,7 +102,7 @@ replaceinfo(char *src, struct account *a, struct action *t, char *s)
 {
 	char		*map[REPL_LEN];
 	    
-	memset(map, 0, sizeof map);
+	memset(map, 0, REPL_LEN * sizeof (char *));
 	initmap(map, a, t, s);
 
 	return (replace(src, map));
