cbaf9f716e15bd6d880347fff94e16c4864c95bf
Nicholas Marriott <nicholas.marriott@gmail.com>
2007-09-24 15:09:59 +0000

    Missed out this....

---
+++ regress/tests/test-string7.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# $Id: test-string7.sh,v 1.1 2007-09-24 15:09:59 nicm Exp $
+
+. ./test.subr && test_init
+
+cat <<EOF|test_in
+Message-Id: test
+
+EOF
+
+cat <<EOF|test_out
+test
+EOF
+
+cat <<EOF|test_run
+match not string "%[message_id]" !~ "t..t" action rewrite "echo test" continue
+EOF
